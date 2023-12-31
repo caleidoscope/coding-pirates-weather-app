4af5ccbd0e91436213b52fdb922e2793f38acffd
karsten zakarias <k_zakarias@hotmail.com>
2023-12-31 12:19:57 +0000

    with card

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.addEventListener('load',()=>{
     console.log('it works!')
 })
 
-function getWeather(){
+async function getWeather(){
     let cityInput = document.getElementById('cityInput').value;
 
     if (cityInput === '') {
@@ -12,30 +12,25 @@ function getWeather(){
 
     const geoApiUrl = `https://geocoding-api.open-meteo.com/v1/search?name=${cityInput}&count=1&language=en&format=json`
 
-    axios.get(geoApiUrl)
-    .then(
-        response=>
-        {
-            result = response.data.results[0]
-            console.log(result)
+    try {
+        const response = await axios.get(geoApiUrl)
+        result = response.data.results[0]
+        console.log(result)
 
-            let name = result.name
-            let lat = result.latitude
-            let lon = result.longitude
+        let name = result.name
+        let lat = result.latitude
+        let lon = result.longitude
 
-            const weatherApiUrl = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,apparent_temperature,weather_code&timezone=Europe%2FCopenhagen`
-    
-            axios.get(weatherApiUrl)
-            .then(
-                response=>
-                {
-                    console.log(response)
-                }
-            )
-        })
-    .catch(
-        err=>console.error(err)
-    )
+        const weatherApiUrl = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,apparent_temperature,weather_code&timezone=Europe%2FCopenhagen`
+        
+        try {
+            const response = await axios.get(weatherApiUrl)
+            console.log(response)
+        } catch (err) {
+            console.error(err)
+        }
 
-    
+    } catch(err) {
+        console.error(err)
+    }
 }
